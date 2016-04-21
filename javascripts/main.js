589e308269d71534e2d3fcec20a8290f57fc7e48
Jordan Liu <jminjie@umich.edu>
2016-04-21 16:14:06 -0400

    Grab form fields with js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,4 +2,8 @@ console.log('This would be the main JS file.');
 
 function printHello() {
 	console.log("prints hello");
+	var password = document.getElementById('password');
+	var uniq = document.getElementById('login');
+	console.log(password.value);
+	console.log(uniq.value);
 }
